2d855aa59cd7bec53ce607ccf8b76fe3c2b99dba
anfastk13@gmail.com <anfastk13@gmail.com>
2025-03-09 21:44:07 +0530

    Unwanted Files Removed

--- utils/helper/discountCalculation.go
+++ utils/helper/discountCalculation.go
@@ -14,36 +14,30 @@ func DiscountCalculation(productID uint, categoryID uint, regularPrice float64, 
 	var productOffer models.ProductOffer
 	var categoryOffer models.OfferByCategory
 
-	// Fetch Product Offer
 	if err := config.DB.First(&productOffer, "product_id = ? AND status = ?", productID, "Active").Error; err != nil {
-		productOffer.OfferPercentage = 0 // Default to zero if no offer found
+		productOffer.OfferPercentage = 0 
 	}
 
-	// Fetch Category Offer
 	if err := config.DB.First(&categoryOffer, "category_id = ? AND offer_status = ?", categoryID, "Active").Error; err != nil {
-		categoryOffer.CategoryOfferPercentage = 0 // Default to zero if no offer found
+		categoryOffer.CategoryOfferPercentage = 0 
 	}
 
-	// Determine the highest discount percentage
 	discountPercentage := productOffer.OfferPercentage
 	if categoryOffer.CategoryOfferPercentage > productOffer.OfferPercentage {
 		discountPercentage = categoryOffer.CategoryOfferPercentage
 	}
 
-	// Calculate discount amount
 	discountAmount := 0.0
 	if discountPercentage > 0 {
 		discountAmount = regularPrice * discountPercentage / 100
 	}
 
-	// Calculate product discount percentage based on sale price
 	difference := regularPrice - salePrice
 	productDiscountPercentage := 0.0
 	if regularPrice > 0 {
 		productDiscountPercentage = (difference / regularPrice) * 100
 	}
 
-	// Total Discount Percentage
 	totalPercentage := productDiscountPercentage + discountPercentage
 
 	return discountAmount, totalPercentage, nil
