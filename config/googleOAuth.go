1d6e92bb5105c9f21b78e27c3227a3fbc36758f1
anfastk13@gmail.com <anfastk13@gmail.com>
2025-03-25 20:20:57 +0530

    redirect url updated

--- config/googleOAuth.go
+++ config/googleOAuth.go
@@ -15,7 +15,7 @@ func InitializeGoogleOAuth() {
 	GoogleOAuthConfig = &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://laptix.ddns.net/auth/google/callback",
+		RedirectURL:  "https://laptix.ddns.net/auth/google/callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
