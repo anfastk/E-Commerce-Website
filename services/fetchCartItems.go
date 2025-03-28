67e6e4431bedad0956cf760e61de14990858e330
anfastk13@gmail.com <anfastk13@gmail.com>
2025-03-28 21:22:41 +0530

    User Cart  Ui Updated

--- services/fetchCartItems.go
+++ services/fetchCartItems.go
@@ -33,17 +33,17 @@ func FetchCartItems(userID uint) (models.Cart, []CartItemDetailWithDiscount, err
 			First(&productDetail, item.ProductVariantID).Error; err != nil {
 			return cart, []CartItemDetailWithDiscount{}, errors.New("Error fetching cart items")
 		}
-		var productImage models.ProductVariantsImage
+		var productImage []models.ProductVariantsImage
 		if err := config.DB.Select("product_variants_images").
-			Where("product_variant_id = ?", item.ProductID).
-			First(&productImage).Error; err != nil {
+			Where("product_variant_id = ?", item.ProductVariantID).
+			Find(&productImage).Error; err != nil {
 			return cart, []CartItemDetailWithDiscount{}, errors.New("Product Image Not Found")
 		}
 		discountAmount, _, _ := helper.DiscountCalculation(productDetail.ID, productDetail.CategoryID, productDetail.RegularPrice, productDetail.SalePrice)
 		cartItemsDetails = append(cartItemsDetails, CartItemDetailWithDiscount{
 			CartItem:       item,
 			ProductDetails: productDetail,
-			ProductImage:   productImage.ProductVariantsImages,
+			ProductImage:   productImage[0].ProductVariantsImages,
 			DiscountPrice:  productDetail.SalePrice - discountAmount,
 		})
 	}
