5ae90c93705831fef14b3a54836e3e58cb4284ad
anfastk13@gmail.com <anfastk13@gmail.com>
2025-04-03 14:22:51 +0530

    wishlist Cart Bug Fixed

--- controllers/user/wishlistHandler.go
+++ controllers/user/wishlistHandler.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
@@ -59,13 +60,19 @@ func ShowWishlist(c *gin.Context) {
 	var wishlistResponse []response
 	for _, item := range wishlistItems {
 		var cart models.Cart
-		if err := config.DB.First(&cart, "user_id = ?", userID).Error; err != nil {
-			logger.Log.Error("Cart not found",
-				zap.Uint("userID", userID),
-				zap.Error(err))
-			helper.RespondWithError(c, http.StatusNotFound, "Cart Not Found", "Something Went Wrong", "")
+	if err := config.DB.First(&cart, "user_id = ?", userID).Error; err != nil {
+		cart = models.Cart{
+			UserID: userID,
+		}
+		if createErr := config.DB.Create(&cart).Error; createErr != nil {
+			logger.Log.Error("Failed to create cart", zap.Uint("userID", userID), zap.Error(createErr))
+			config.DB.Rollback()
+			err := errors.New("Cart creation Failed")
+			c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
+		logger.Log.Info("Created new cart", zap.Uint("cartID", cart.ID))
+	}
 
 		isInCart := true
 		var cartItem models.CartItem
