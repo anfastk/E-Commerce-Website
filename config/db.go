cdbe8bccb005702c4fa1bedc7bdb25b93ba7bca3
anfastk13@gmail.com <anfastk13@gmail.com>
2025-07-23 05:17:02 +0530

    Database Details Updated

--- config/db.go
+++ config/db.go
@@ -1,8 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"os"
- 
+
 	"github.com/anfastk/E-Commerce-Website/pkg/logger"
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
@@ -16,19 +17,29 @@ var (
 )
 
 func DBconnect() {
-	var err error
-	dsn := os.Getenv("DB")
+    var err error
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		logger.Log.Error("Database connection failed", zap.Error(err))
-		IsConfigErr = false
-		ConfigErr = err
-		return
-	}
+    dbHost := os.Getenv("DB_HOST")     // "postgres-service"
+    dbUser := os.Getenv("DB_USER")
+    dbPass := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+    dbPort := os.Getenv("DB_PORT")
 
-	logger.Log.Info("Database connected successfully")
-	IsConfigErr = true
-	ConfigErr = nil
-	return
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
+        dbHost, dbUser, dbPass, dbName, dbPort,
+    )
+
+    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        logger.Log.Error("Database connection failed", zap.Error(err))
+        IsConfigErr = false
+        ConfigErr = err
+        return
+    }
+
+    logger.Log.Info("Database connected successfully")
+    IsConfigErr = true
+    ConfigErr = nil
 }
+
